1a3b5471b267af25aca5d468e4b7143f340b944f
Jalihal <amogh.jalihal@gmail.com>
2018-04-28 22:43:35 -0400

    updated analysis files to run for three versions of models.

--- src/diet-analysis/essential-reaction-finder.py
+++ src/diet-analysis/essential-reaction-finder.py
@@ -8,61 +8,71 @@ from tqdm import tqdm
 ListOfRandomSpecies = [
 'Bacteroides_thetaiotaomicron_VPI_5482',
 'Bacteroides_sp_2_2_4',
-# 'Parabacteroides_johnsonii_DSM_18315',
-# 'Prevotella_oralis_ATCC_33269',
-# 'Eubacterium_eligens_ATCC_27750',
-# 'Slackia_exigua_ATCC_700122',
-# 'Dorea_longicatena_DSM_13814',
-# 'Clostridium_bartlettii_DSM_16795',
-# 'Streptococcus_sp_I_P16',
-# 'Blautia_hydrogenotrophica_DSM_10507',
-# 'Brevundimonas_bacteroides_DSM_4726',
-# 'Clostridium_hylemonae_DSM_15053',
-# 'Sutterella_wadsworthensis_3_1_45B',
-# # # 'Enterobacteriaceae_bacterium_9_2_54FAA',
-# # 'Bacillus_megaterium_DSM319',
-# # 'Peptostreptococcus_stomatis_DSM_17678',
-# # 'Brachybacterium_paraconglomeratum_LC44',
-# 'Neisseria_elongata_subsp_glycolytica_ATCC_29315',
-# 'Rothia_aeria_F0474',
-# 'Staphylococcus_hominis_subsp_hominis_C80', 
+'Parabacteroides_johnsonii_DSM_18315',
+'Prevotella_oralis_ATCC_33269',
+'Eubacterium_eligens_ATCC_27750',
+'Slackia_exigua_ATCC_700122',
+'Dorea_longicatena_DSM_13814',
+'Clostridium_bartlettii_DSM_16795',
+'Streptococcus_sp_I_P16',
+'Blautia_hydrogenotrophica_DSM_10507',
+'Brevundimonas_bacteroides_DSM_4726',
+'Clostridium_hylemonae_DSM_15053',
+'Sutterella_wadsworthensis_3_1_45B',
+'Enterobacteriaceae_bacterium_9_2_54FAA',
+'Bacillus_megaterium_DSM319',
+'Peptostreptococcus_stomatis_DSM_17678', # average-european-diet causes problems
+'Brachybacterium_paraconglomeratum_LC44',
+'Neisseria_elongata_subsp_glycolytica_ATCC_29315',
+'Rothia_aeria_F0474',
+'Staphylococcus_hominis_subsp_hominis_C80', 
 ]
 
 betternames = [n.split('_')[0][0] + '.' + n.split('_')[1] for n in ListOfRandomSpecies]
 stats = {}
-types = ['western-diet', 'average-european-diet', 'high-fiber-diet', 'unconstrained']
+types = [
+    'high-fiber-diet',
+    'unconstrained',
+    'western-diet',
+#    'average-european-diet',
+ 
+]
 for t in types:
+    print(t)
     for sp in ListOfRandomSpecies:
-        if sp not in stats.keys():
-            stats[sp] = {}
-        print(sp)
-
-        model = cobra.io.read_sbml_model("../../dfba/data/"+ t +"/" + sp + ".xml")
-        AllExchanges = [r.id for r in model.exchanges]
-        solution = model.optimize()
-        igr = solution.objective_value
-        print("\tInitial growth rate:" + str(igr))
-        DietSensitivity = {}
-        
-        count = 0
-        for r in AllExchanges:
-            if r not in DietSensitivity.keys():
-                originallb = model.reactions.get_by_id(r).lower_bound
-                model.reactions.get_by_id(r).lower_bound = 0.0
-                s = model.optimize()
-                gr = s.objective_value
-                if abs(igr-gr)/igr > 0.01:
-                    DietSensitivity[r] = abs(igr-gr)/igr
-                    model.reactions.get_by_id(r).lower_bound = originallb
-            count += 1
-
-        solution = model.optimize()
-        
-        print("\tWith minimal influxes: " + str(solution.objective_value))
-        print("\tThe minimal set contains " + str(len(DietSensitivity.keys())) + " metabolites")
-
-        stats[sp][t]=len(DietSensitivity.keys())
-        
+        if 'Peptostreptococcus' in sp and t=='average-european-diet':
+            continue
+        else:
+            if sp not in stats.keys():
+                stats[sp] = {}
+            print(sp)
+    
+            model = cobra.io.read_sbml_model("../../dfba/data/"+ t +"/" + sp + ".xml")
+            AllExchanges = [r.id for r in model.exchanges]
+            solution = model.optimize()
+            igr = solution.objective_value
+            print("\tInitial growth rate:" + str(igr))
+            DietSensitivity = {}
+            
+            count = 0
+            for r in AllExchanges:
+                if r not in DietSensitivity.keys():
+                    originallb = model.reactions.get_by_id(r).lower_bound
+                    model.reactions.get_by_id(r).lower_bound = 0.0
+                    s = model.optimize()
+                    gr = s.objective_value
+                    if abs(igr-gr)/igr > 0.01:
+                        DietSensitivity[r] = abs(igr-gr)/igr
+                        model.reactions.get_by_id(r).lower_bound = originallb
+                count += 1
+    
+            solution = model.optimize()
+            
+            print("\tWith minimal influxes: " + str(solution.objective_value))
+            print("\tThe minimal set contains " + str(len(DietSensitivity.keys())) + " metabolites")
+    
+            stats[sp][t]=len(DietSensitivity.keys())
+            
 for s in stats:
     print(s + '\t' + str(stats[s][types[0]]) + '\t' +  str(stats[s][types[1]]) + '\t' +  str(stats[s][types[2]]))
 
--- src/diet-analysis/individual_species_responses.py
+++ src/diet-analysis/individual_species_responses.py
@@ -1,43 +1,72 @@
+import os
+import sys
 import pandas as pd
-#RELPATH = os.getcwd()
-#SRCPATH = RELPATH + "/../../src/"
-#sys.path.append(SRCPATH)
-import cobra
-#import simulator
+RELPATH = os.getcwd()
+SRCPATH = RELPATH + "/../../src/"
+sys.path.append(SRCPATH)
+import simulator
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 
-ListOfRandomSpecies = [
-'Bacteroides_thetaiotaomicron_VPI_5482',
-'Bacteroides_sp_2_2_4',
-'Parabacteroides_johnsonii_DSM_18315',
-'Prevotella_oralis_ATCC_33269',
-'Eubacterium_eligens_ATCC_27750',
-'Slackia_exigua_ATCC_700122',
-'Dorea_longicatena_DSM_13814',
-'Clostridium_bartlettii_DSM_16795',
-'Streptococcus_sp_I_P16',
-'Blautia_hydrogenotrophica_DSM_10507',
-'Brevundimonas_bacteroides_DSM_4726',
-'Clostridium_hylemonae_DSM_15053',
-'Sutterella_wadsworthensis_3_1_45B',
-'Enterobacteriaceae_bacterium_9_2_54FAA',
-'Bacillus_megaterium_DSM319',
-'Peptostreptococcus_stomatis_DSM_17678',
-'Brachybacterium_paraconglomeratum_LC44',
-'Neisseria_elongata_subsp_glycolytica_ATCC_29315',
-'Rothia_aeria_F0474',
-'Staphylococcus_hominis_subsp_hominis_C80',
+ModelDefinitions = ['high-fiber-diet',
+                    'western-diet',
+                    #'average-european-diet',
+                    'unconstrained']
+
+ListOfMostAbundantSpecies = [
+    'Bacteroides_thetaiotaomicron_VPI_5482',
+    'Bacteroides_sp_2_2_4',
+    'Parabacteroides_johnsonii_DSM_18315',
+    'Prevotella_oralis_ATCC_33269',
+    'Eubacterium_eligens_ATCC_27750',
+    'Slackia_exigua_ATCC_700122',
+    'Dorea_longicatena_DSM_13814',
+    'Clostridium_bartlettii_DSM_16795',
+    'Streptococcus_sp_I_P16',
+    'Blautia_hydrogenotrophica_DSM_10507',
+    'Brevundimonas_bacteroides_DSM_4726',
+    'Clostridium_hylemonae_DSM_15053',
+    'Sutterella_wadsworthensis_3_1_45B',
+    'Enterobacteriaceae_bacterium_9_2_54FAA',
+    'Bacillus_megaterium_DSM319',
+    'Peptostreptococcus_stomatis_DSM_17678', # average-european-diet causes problems
+    'Brachybacterium_paraconglomeratum_LC44',
+    'Neisseria_elongata_subsp_glycolytica_ATCC_29315',
+    'Rothia_aeria_F0474',
+    'Staphylococcus_hominis_subsp_hominis_C80'
 ]
+i = 0
 
-for sp in ListOfRandomSpecies:
-    print("Reading file " + sp)
-    model = cobra.io.read_sbml_model("../../dfba/data/average-european-diet/" + sp + ".xml")
-    AllExchanges = [r.id for r in model.exchanges]
-    print("\tThe number of exchange reactions are " + str(len(AllExchanges)))
-    EssentialGenes = [r.id for r in cobra.flux_analysis.variability.find_essential_genes(model)]
-    print("\tThe number of essential genes are " + str(len(EssentialGenes)))
-    Intersection = [rxn for rxn in EssentialGenes if rxn in AllExchanges]
-    print("\tEssential exchanges for " + sp + " are:")
-    for i in Intersection:
-        print(i)
+
+DietDict = {
+    'HighFiber': '../../dfba/data/diet-definitions/VMH_HighFiber.tsv',
+    'HighProtein': '../../dfba/data/diet-definitions/VMH_HighProtein.tsv',
+    'Vegetarian': '../../dfba/data/diet-definitions/VMH_Vegetarian.tsv',
+    'Vegan': '../../dfba/data/diet-definitions/VMH_Vegan.tsv',
+    'Type2Diabetes': '../../dfba/data/diet-definitions/VMH_Type2Diabetes.tsv',
+    'Unhealthy': '../../dfba/data/diet-definitions/VMH_Unhealthy.tsv'
+}
+
+def characterizeSingleSpecies(ModelType, SingleSpeciesDict, DietDict):
+    plt.figure()
+    sp_id = SingleSpeciesDict.keys()[0]
+    for diet in DietDict.keys():
+        print('\t\t' + diet)
+        DietDF = pd.read_csv(DietDict[diet], sep='\t')
+        Output, SpecDict, Definition = simulator.simulateCommunity(SingleSpeciesDict, DietDF, MaxIter=20, cobraonly=True)
+        T = []
+        Biomass = []
+        for P in Output:
+            T += list(P['t'])
+            Biomass += list(P[SpecDict[sp_id]['Name']])
+        plt.plot(T, Biomass, label=diet)
+    plt.legend()
+    plt.title(SingleSpeciesDict[sp_id]['Name'])
+    plt.xlabel('min')
+    plt.ylabel('gdw')
+    plt.savefig('./figs/'+ ModelType + '_'  + SingleSpeciesDict[sp_id]['Name'] + '.pdf', dpi=300)
+
+for sp in ListOfMostAbundantSpecies:
+    print(sp)
+    for m in ModelDefinitions:
+        print('\t' + m)
+        characterizeSingleSpecies(m, {'1':{'File':RELPATH + '/../../dfba/data/' + m + '/' + sp + '.xml', 'initAbundance':0.01}}, DietDict)
